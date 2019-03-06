deaa01d03465bd1fd18a08d06606c8b78d3b418d
Regis Ribeiro <rockmaior@gmail.com>
2019-03-05 21:00:34 -0300

    ajuste para criacao das tabelas

--- src/main/java/br/com/casadocodigo/loja/conf/JPAProductionConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/JPAProductionConfiguration.java
@@ -20,7 +20,7 @@ public class JPAProductionConfiguration {
 		Properties properties = new Properties();
 		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
 		properties.setProperty("hibernate.show_sql", "true");
-		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		properties.setProperty("hibernate.hbm2ddl.auto", "create");
 		
 		return properties;
 	}
